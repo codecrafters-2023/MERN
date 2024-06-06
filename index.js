01fef60416bcbfa9955a8c80221b7ad5c1468916
codecrafters <codecrafters@gurmaanitservices.com>
2024-06-07 04:27:51 +0530

    new changes

--- index.js
+++ index.js
@@ -9,39 +9,50 @@ app.use(express.json())
 const port = 8000;
 
 mongoose.connect('mongodb+srv://codecrafters:Devteam2024@data.ma9zn4r.mongodb.net/alldata?retryWrites=true&w=majority&appName=data')
-.then(res => console.log("mongodb connected"))
-.catch(err => console.console.log('Error connecting to MongoDB'))
+    .then(res => console.log("mongodb connected"))
+    .catch(err => console.console.log('Error connecting to MongoDB'))
 
 
-app.post('/add', (req, res) => {
-    const {title, description} = req.body
-    Test.create({
+app.post('/add', async (req, res) => {
+    const { title, description } = req.body
+    await Test.create({
         title, description
     })
-    .then((result) => res.json(result))
-    .catch((err) => res.json(err))
+        .then((result) => res.json(result))
+        .catch((err) => res.json(err))
 })
 
-app.get('/get', (req, res) =>{
-    Test.find()
-    .then((result) => res.json(result))
-    .catch((err) => res.json(err))
+app.get('/get', async (req, res) => {
+    await Test.find()
+        .then((result) => res.json(result))
+        .catch((err) => res.json(err))
 })
 
-app.delete('/delete/:id', (req, res) =>{
+app.get('/getSingleData/:id', async (req, res) => {
     const id = req.params.id
-    Test.findByIdAndDelete(id)
-    .then((result) => res.json(result))
-    .catch((err) => res.json(err))
+    // console.log(id);
+    await Test.findById({ _id: id })
+        .then((result) => res.json(result))
+        .catch((err) => res.json(err))
 })
 
-app.put('/update/:id', (req, res) =>{
+app.delete('/delete/:id', async (req, res) => {
     const id = req.params.id
-    Test.findByIdAndUpdate({_id: id} , {status: true})
-    .then((result) => res.json(result))
-    .catch((err) => res.json(err))
+    await Test.findByIdAndDelete(id)
+        .then((result) => res.json(result))
+        .catch((err) => res.json(err))
 })
 
+app.put('/update/:id',async (req, res) => {
+    const id = req.params.id
+    console.log(id);
+    const { title, description } = req.body;
+    console.log(title, description);
+    await Test.findOneAndUpdate({ _id: id }, { title: title, description: description })
+        .then((result) => res.json(result))
+        .catch((err) => res.json(err))
+
+})
 
 app.listen(port, () => {
     console.log("Server listening on " + port);
